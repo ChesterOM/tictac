091c6d6cd6fbd87916b4d655b034d396eb068fac
ChesterOM <131749381+ChesterOM@users.noreply.github.com>
2023-07-24 20:03:29 +1200

    Implement basic gameplay functions

--- game.js
+++ game.js
@@ -7,19 +7,38 @@
             gameBoard.forEach((square, index) => {
                 boardHTML += `<div class="square" id="square-${index}">${square}</div>`
             });
-
             document.querySelector("#gameboard").innerHTML = boardHTML;
+
+            const squares = document.querySelectorAll(".square");
+            squares.forEach((square) => {
+                square.addEventListener('click',GameController.handleClick);
+            });
         }
-        
+
+        const update = (index, value) => {
+            gameBoard[index] = value;
+            render()
+        }
+
         return {
-            render
+            render,
+            update,
         }
     })();
 
     const GameController = (() => {
         let players = [];
         let currentPlayer;
-        let gameOver = false;
+        let gameOver;
+
+        const handleClick = (event) => {
+            let index = parseInt(event.target.id.split("-")[1]);
+            console.log(index);
+
+            GameBoard.update(index, currentPlayer);
+            currentPlayer = currentPlayer === players[0] ? players[1] : players[0]
+        }
+
 
         const start = () => {
             players = [document.querySelector("#select-player").value,
@@ -27,18 +46,18 @@
             
             currentPlayer = players[0];
             console.log(players, currentPlayer);
-            GameBoard.render()
             gameOver = false;
+            GameBoard.render();
         }
-
         return{
-            start
-        }
+            start,
+            handleClick,
+        };
     })();
 
     const startButton = document.querySelector("#start");
     startButton.addEventListener("click", () => {
-        GameController.start()
+        GameController.start();
     });
 })();
 
